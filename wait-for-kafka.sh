cfbfc6aa87ec5c008b7cefecd77994e9ece53861
Nagiyev Mahaddin <nmeheddin9@gmail.com>
2025-09-14 17:36:34 +0400

    Initial commit

---
+++ wait-for-kafka.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -e
+
+# Параметры Kafka
+KAFKA_BROKER_LIST="kafka-0:9092,kafka-1:9092"
+
+# Функция проверки доступности Kafka
+check_kafka() {
+  echo "Checking Kafka availability..."
+  until kafka-topics.sh --bootstrap-server $KAFKA_BROKER_LIST --list &>/dev/null; do
+    echo "Kafka is not available yet. Waiting..."
+    sleep 5
+  done
+  echo "Kafka is available."
+}
+
+# Выполнение функции проверки
+check_kafka
